40849430e6a9f3ed8518a5d9ec79d9b0492eff2a
aarostami <alireza.ro76@gmail.com>
2024-10-18 15:36:21 +0330

    commit

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -12,12 +12,12 @@ export default function Footer() {
 			<Container sx={{ display: 'flex', justifyContent: 'space-between', py: '1rem' }}>
 				<Image src={footlogo} style={{ width: '5%', height: '10%' }} alt='foo' />
 				<ul style={{ listStyleType: 'none' }}>
-					<li className={style.footerlist}><Link href='./page.tsx'>Home</Link></li>
-					<li className={style.footerlist}><Link href='#'>Documentation</Link></li>
+					<li className={style.footerlist}><Link href='./page.tsx' style={{backgroundColor: 'black'}}>Home</Link></li>
+					<li className={style.footerlist}><Link href='#' style={{backgroundColor: 'black'}}>Documentation</Link></li>
 					<li className={style.footerlist}><Button variant="outlined" color="success">Purchase now</Button></li>
 				</ul>
 			</Container>
-			<p style={{ textAlign: 'center' }}>© Webbee. 2021, Maccarian. All rights reserved</p>
+			<p style={{ textAlign: 'center', backgroundColor: 'black' }}>© Webbee. 2021, Maccarian. All rights reserved</p>
 		</footer>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
+  return (	
     <html lang="en">
       <body className={inter.className} style={{backgroundColor: '#f7f9fc'}}>{children}</body>
     </html>
